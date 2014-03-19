b72c69a8dab00fb37777ec4ddd10a6b0bfc448ec
George Clam <george.clam@yahoo.com>
2014-03-19 11:35:37 -0600

    primary "Hello world" test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DavinciProject::Application.routes.draw do
+
+  get "/" => "main#root"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
