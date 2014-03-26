37af35539461a271d0d82769527060189218bc62
George Clam <george.clam@yahoo.com>
2014-03-26 01:23:01 -0600

    hard-coded html for index.html.erb page. Starting to build on my idea and really grasp where I want to go. First table will be "works" consisting of title, author, description, availability. Second will be "Artists" with location, contact info, will tie to works database Try and keep the site simple and clean, and put a strong focus on functionality for demonstration purposes. That's what Rails is all about.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,5 +3,25 @@ class MainController < ApplicationController
   def root
   	render :index and return
   end
+  
+  def drawings
+  	render :drawings and return
+  end
 
+  def paintings
+  	render :paintings and return
+  end
+
+  def showcase
+  	render :showcase and return
+  end
+
+  def graphic_art
+  	render :music and return
+  end
+
+  def sign_in
+  	render :mailing_list and return
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,20 @@
 DavinciProject::Application.routes.draw do
 
-  get "/" => "main#root"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get  "/"                   => "main#root"
+  post "/"                   => "main#root"
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get  "/drawings"           => "main#drawings"
+  get  "/drawings/:title"    => "main#showcase"
+  post "/drawings/:title"    => "main#showcase"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get  "/paintings"          => "main#paintings"
+  get  "/paintings/:title"   => "main#showcase"
+  post "/paintings/:title"   => "main#showcase"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get  "/graphic_art"        => "main#graphic_art"
+  get  "/graphic_art/:title" => "main#showcase"
+  
+  get  "/sign_in"            => "main#mailing_list"
+  post "/sign_in"            => "main#mailing_list"
+  
 end
