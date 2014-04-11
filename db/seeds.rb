4c361098c83968a29a0e26e6311cbdb0849332ea
George Clam <george.clam@yahoo.com>
2014-04-10 18:10:23 -0600

    Git push 4-10-2014

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,27 +64,235 @@ Work.create! \
   artist_id: 1,
   availability: false,
   genre: "drawing",
-  work_filename: "/zack_low_works/i_speak_tree.jpg"
+  work_filename: "zack_low_works/i_speak_tree.jpg"
 
 Work.create! \
   title: "I hate reflections",
   artist_id: 1,
   availability: false,
   genre: "drawing",
-  work_filename: "/zack_low_works/i_hate_reflections.jpg"
+  work_filename: "zack_low_works/i_hate_reflections.jpg"
 
 Work.create! \
   title: "Handy with tools",
   artist_id: 1,
   availability: false,
   genre: "drawing",
-  work_filename: "/zack_low_works/handy_with_tools.jpg"
+  work_filename: "zack_low_works/handy_with_tools.jpg"
 
 Work.create! \
   title: "Colors",
   artist_id: 1,
   availability: false,
   genre: "drawing",
-  work_filename: "/zack_low_works/colors.jpg"
+  work_filename: "zack_low_works/colors.jpg"
 
+Work.create! \
+  title: "Analysis",
+  artist_id: 2,
+  availability: false,
+  genre: "drawing",
+  work_filename: "thomas_bloomberg_works/analysis.jpg"
 
+Work.create! \
+  title: "Bang Your Head",
+  artist_id: 2,
+  availability: true,
+  genre: "drawing",
+  work_filename: "thomas_bloomberg_works/bang_your_head.jpg"
+
+Work.create! \
+  title: "National Geographic",
+  artist_id: 2,
+  availability: false,
+  genre: "drawing",
+  work_filename: "thomas_bloomberg_works/national_geographic.jpg"
+
+Work.create! \
+  title: "Overwhelming Darkness",
+  artist_id: 2,
+  availability: false,
+  genre: "painting",
+  work_filename: "thomas_bloomberg_works/overwhelming_darkness.jpg"
+
+Work.create! \
+  title: "Zombie Love Tribute",
+  artist_id: 2,
+  availability: false,
+  genre: "painting",
+  work_filename: "thomas_bloomberg_works/zombie_love_tribute.jpg"  
+
+Work.create! \
+  title: "All-Knowing Cosmic Owl",
+  artist_id: 3,
+  availability: false,
+  genre: "painting",
+  work_filename: "elenas_weird_works/all_knowing_cosmic_owl.jpg"  
+
+Work.create! \
+  title: "Bad Lands",
+  artist_id: 3,
+  availability: false,
+  genre: "painting",
+  work_filename: "elenas_weird_works/bad_lands.jpg"  
+
+Work.create! \
+  title: "Dark Horse",
+  artist_id: 3,
+  availability: false,
+  genre: "painting",
+  work_filename: "elenas_weird_works/dark_horse.jpg"
+
+Work.create! \
+  title: "Fly Girl",
+  artist_id: 3,
+  availability: false,
+  genre: "painting",
+  work_filename: "elenas_weird_works/fly_girl.jpg"  
+
+Work.create! \
+  title: "I spy",
+  artist_id: 3,
+  availability: false,
+  genre: "painting",
+  work_filename: "elenas_weird_works/i_spy.jpg"  
+
+Work.create! \
+  title: "Home Faker",
+  artist_id: 3,
+  availability: false,
+  genre: "painting",
+  work_filename: "elenas_weird_works/home_faker.jpg"  
+
+Work.create! \
+  title: "Behind the Scenes",
+  artist_id: 4,
+  availability: false,
+  genre: "graphic art",
+  work_filename: "joshua_guisinger_works/behind_the_scenes.jpg" 
+
+Work.create! \
+  title: "League of Shadows",
+  artist_id: 4,
+  availability: false,
+  genre: "graphic art",
+  work_filename: "joshua_guisinger_works/league_of_shadows.jpg" 
+
+Work.create! \
+  title: "Faceless",
+  artist_id: 4,
+  availability: false,
+  genre: "graphic art",
+  work_filename: "joshua_guisinger_works/faceless.jpg" 
+
+Work.create! \
+  title: "Listen",
+  artist_id: 4,
+  availability: false,
+  genre: "drawing",
+  work_filename: "joshua_guisinger_works/listen.jpg" 
+
+Work.create! \
+  title: "Thom Yorke",
+  artist_id: 4,
+  availability: false,
+  genre: "drawing",
+  work_filename: "joshua_guisinger_works/thom_yorke.jpg" 
+
+Work.create! \
+  title: "Girl",
+  artist_id: 4,
+  availability: false,
+  genre: "painting",
+  work_filename: "joshua_guisinger_works/girl.jpg" 
+
+Work.create! \
+  title: "Untitled",
+  artist_id: 4,
+  availability: false,
+  genre: "painting",
+  work_filename: "joshua_guisinger_works/untitled.jpg" 
+
+Work.create! \
+  title: "Untitled",
+  artist_id: 4,
+  availability: false,
+  genre: "painting",
+  work_filename: "joshua_guisinger_works/untitled2.jpg"
+
+Work.create! \
+  title: "Alien",
+  artist_id: 5,
+  availability: false,
+  genre: "graphic art",
+  work_filename: "ken_kokoszka_works/alien.jpg"  
+
+Work.create! \
+  title: "Camel",
+  artist_id: 5,
+  availability: false,
+  genre: "drawing",
+  work_filename: "ken_kokoszka_works/camel.jpg"  
+
+Work.create! \
+  title: "Choice Encounter",
+  artist_id: 5,
+  availability: false,
+  genre: "graphic art",
+  work_filename: "ken_kokoszka_works/choice_encounter.jpg"  
+
+Work.create! \
+  title: "Goblin",
+  artist_id: 5,
+  availability: false,
+  genre: "graphic art",
+  work_filename: "ken_kokoszka_works/goblin.jpg"  
+
+Work.create! \
+  title: "Lion",
+  artist_id: 5,
+  availability: false,
+  genre: "drawing",
+  work_filename: "ken_kokoszka_works/lion.jpg"  
+
+Work.create! \
+  title: "Phobia",
+  artist_id: 5,
+  availability: false,
+  genre: "drawing",
+  work_filename: "ken_kokoszka_works/phobia.jpg"  
+
+Work.create! \
+  title: "Redcap",
+  artist_id: 5,
+  availability: false,
+  genre: "graphic art",
+  work_filename: "ken_kokoszka_works/redcap.jpg"  
+
+Work.create! \
+  title: "Scorpion",
+  artist_id: 5,
+  availability: false,
+  genre: "drawing",
+  work_filename: "ken_kokoszka_works/scorpion.jpg" 
+
+Work.create! \
+  title: "Gold Bracelet",
+  artist_id: 6,
+  availability: false,
+  genre: "misc",
+  work_filename: "alexandra_kinney_works/gold_bracelet.jpg" 
+
+Work.create! \
+  title: "Spike Necklace",
+  artist_id: 6,
+  availability: false,
+  genre: "misc",
+  work_filename: "alexandra_kinney_works/spike_necklace.jpg" 
+
+Work.create! \
+  title: "Vintage-Inspired Necklace",
+  artist_id: 6,
+  availability: false,
+  genre: "misc",
+  work_filename: "alexandra_kinney_works/vintage_inspired_necklace.jpg" 
